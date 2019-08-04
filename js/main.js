3658deace30c45baaedf8290eae09f7adb64cc49
bojan30 <bojancuk33@gmail.com>
2019-08-04 22:36:47 +0200

    .

--- js/main.js
+++ js/main.js
@@ -1,6 +1,44 @@
-//toggle question button
+//elements
+const buttonOne = document.querySelector('#first-step-button');
+const buttonTwo = document.querySelector('#second-step-button');
+const phoneInput = document.querySelector('#second-step');
+const privacy = document.querySelector("#privacy");
+const firstStepNumber = document.querySelector('#first-step-number');
+const secondStepNumber = document.querySelector('#second-step-number');
+const toggleButtons = document.querySelectorAll('.toggle-custom');
+const scrollLink = document.querySelector('#scroll-link');
 
-let toggleButtons = document.querySelectorAll('.toggle-custom');
+//parallax effect(landing section)
+
+function parallax(target, amount) {
+  let scroll = window.pageYOffset;
+  let el = document.querySelector(target);
+  el.style.backgroundPositionY = (amount - 1) * scroll + "px";
+}
+window.addEventListener('scroll',()=>{
+  parallax('#landing-section', 0.7);
+});
+
+//form steps
+
+buttonOne.addEventListener('click', stepOne);
+buttonTwo.addEventListener('click', stepTwo);
+
+function stepOne(e){
+  e.preventDefault();
+  phoneInput.classList.add('active');
+  buttonTwo.classList.add('active');
+  privacy.style.display = "none";
+  firstStepNumber.classList.add('active');
+  this.style.display = "none";
+}
+
+function stepTwo(e){
+  e.preventDefault();
+  secondStepNumber.classList.add("active");
+}
+
+//toggle question button
 
 toggleButtons.forEach((button,index)=>{
   button.addEventListener('click',()=>{
@@ -20,3 +58,38 @@ toggleButtons.forEach((button,index)=>{
     }
   })
 })
+
+//smooth scroll to steps section
+
+function scroll(target, duration){
+  //animation target(destination)
+  const el = document.querySelector(target);
+  //scroll amount
+  const scrollAmount = window.pageYOffset;
+  //distance from the top
+  const position = el.getBoundingClientRect().top + scrollAmount;
+  //animation distance
+  const animationDistance = position - scrollAmount;
+  let startTime = null;
+  function animate(time){
+    if(startTime === null) startTime = time;
+    let timeElapsed = time - startTime;
+    let run = easing(timeElapsed, scrollAmount, animationDistance, duration);
+    window.scrollTo(0, run);
+    if(timeElapsed < duration) requestAnimationFrame(animate);
+  }
+  //accelerate half way, than decelerate
+  function easing(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+  };
+  requestAnimationFrame(animate);
+}
+
+scrollLink.addEventListener('click',(e)=>{
+  e.preventDefault();
+  scroll('#steps-section', 800);
+})
+
